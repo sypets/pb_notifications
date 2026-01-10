2f4439a81858ba1c4744b186fd6ec0092ed0419d
Sybille Peters <sypets@gmx.de>
2026-01-10 14:54:28 +0100

    TYPO3 v13 support

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '12.4.2',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-12.4.99',
+            'typo3' => '12.4.0-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
